cf7621fdf6471395ec42b9eb1e5eb9ae3a2e4fa9
Donald Nyingifa <donaldnyingifa@gmail.com>
2019-09-13 13:35:32 +0100

    Updates

---
+++ precache-manifest.f026ab880abf44dcd7d568afafae3103.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = [
+  {
+    "revision": "3a3ffd3696fea8999087bbdcf97d69d2",
+    "url": "/the_venue/static/media/slide_two.3a3ffd36.jpg"
+  },
+  {
+    "revision": "9aa09a50b7eb84e96d522c10dfaa78e5",
+    "url": "/the_venue/static/media/slide_three.9aa09a50.jpg"
+  },
+  {
+    "revision": "84753208e3ace6c36a3a29c731983a69",
+    "url": "/the_venue/static/media/slide_one.84753208.jpg"
+  },
+  {
+    "revision": "39afd44280ba6883126b",
+    "url": "/the_venue/static/js/runtime~main.39afd442.js"
+  },
+  {
+    "revision": "9e789696e78ba124a961",
+    "url": "/the_venue/static/js/main.9e789696.chunk.js"
+  },
+  {
+    "revision": "815c58a2c7ef93f53f17",
+    "url": "/the_venue/static/js/1.815c58a2.chunk.js"
+  },
+  {
+    "revision": "9e789696e78ba124a961",
+    "url": "/the_venue/static/css/main.bc69716b.chunk.css"
+  },
+  {
+    "revision": "c8847bebc0d6bb739e79c6e336bc15d6",
+    "url": "/the_venue/index.html"
+  }
+];
